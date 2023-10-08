09223068cc3982febf05daa64084beb91c88c411
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-08 14:03:09 -0700

    🎨 Format with Styler

--- tests/testthat/test_obtain_goals_by_minutes.R
+++ tests/testthat/test_obtain_goals_by_minutes.R
@@ -1,8 +1,8 @@
-describe("Obtain goals by number",{
+describe("Obtain goals by number", {
   path_america <- "/workdir/tests/data/teams_statistics_2287_2022.json"
   america_statistics <- jsonlite::read_json(path_america)
   it("First example", {
-    expected <- c(8,9,23,24,13,13,4, 0)
+    expected <- c(8, 9, 23, 24, 13, 13, 4, 0)
     obtained <- obtain_goals_by_minutes_from_team_statistics(america_statistics)
     expect_equal(obtained, expected)
   })
