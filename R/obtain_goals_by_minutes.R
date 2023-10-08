cdcd1a8e25ad2653c097c4ab93f307fea1fa8802
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-08 14:34:31 -0700

    ✅ Pass test `obtain_goals_by_minutes_from_paths()`

--- R/obtain_goals_by_minutes.R
+++ R/obtain_goals_by_minutes.R
@@ -1,3 +1,13 @@
+obtain_goals_by_minutes_from_paths <- function(paths) {
+  goals <- 0
+  for (path in paths) {
+    team_stat <- jsonlite::read_json(path)
+    team_goals <- obtain_goals_by_minutes_from_team_statistics(team_stat)
+    goals <- goals + team_goals
+  }
+  return(goals)
+}
+
 obtain_goals_by_minutes_from_team_statistics <- function(team_stat) {
   goals_by_minutes <- team_stat[["response"]][["goals"]][["for"]][["minute"]]
   comprehenr::to_vec(for (goals in goals_by_minutes) .fix_element(goals[["total"]]))
