d5bb2def1c917db9aa68e010b79cfebf62adac7a
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-11 20:44:03 -0700

    ♻️ Extract function `get_variable_if_event_is_goal()`

--- R/obtain_info_from_events.R
+++ R/obtain_info_from_events.R
@@ -7,15 +7,21 @@ get_name_from_goal_events <- function(events) {
 }
 
 get_time_if_event_is_goal <- function(event) {
-  if (event[["type"]] == "Goal") {
-    .return_time_elapsed(event)
-  }
+  get_variable_if_event_is_goal(event, "time_elapsed")
 }
 
 get_name_if_event_is_goal <- function(event) {
+  get_variable_if_event_is_goal(event, "team_name")
+}
+
+get_variable_if_event_is_goal <- function(event, variable) {
   if (event[["type"]] == "Goal") {
-    .return_team_name(event)
+    return_time_name(event, variable)
   }
+}
+
+return_time_name <- function(event, time_or_name) {
+  RETURN_VARIABLE[[time_or_name]](event)
 }
 
 .return_team_name <- function(event) {
@@ -25,3 +31,8 @@ get_name_if_event_is_goal <- function(event) {
 .return_time_elapsed <- function(event) {
   return(event[["time"]][["elapsed"]])
 }
+
+RETURN_VARIABLE <- list(
+  "team_name" = .return_team_name,
+  "time_elapsed" = .return_time_elapsed
+)
