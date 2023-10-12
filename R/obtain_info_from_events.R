28d312f2935d548ed45c30f359af30026ecb331c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-11 20:23:09 -0700

    ♻️ Extract private functions

--- R/obtain_info_from_events.R
+++ R/obtain_info_from_events.R
@@ -8,13 +8,20 @@ get_name_from_goal_events <- function(events) {
 
 get_time_if_event_is_goal <- function(event) {
   if (event[["type"]] == "Goal") {
-    return(event[["time"]][["elapsed"]])
+    .return_time_elapsed(event)
   }
 }
 
 get_name_if_event_is_goal <- function(event) {
   if (event[["type"]] == "Goal") {
-    return(event[["team"]][["name"]])
+    .return_team_name(event)
   }
 }
 
+.return_team_name <- function(event) {
+  return(event[["team"]][["name"]])
+}
+
+.return_time_elapsed <- function(event) {
+  return(event[["time"]][["elapsed"]])
+}
--- tests/testthat/test_obtain_info_from_events.R
+++ tests/testthat/test_obtain_info_from_events.R
@@ -1,20 +1,20 @@
 describe("Obtain goals from events", {
   event_path <- "/workdir/tests/data/data_events_881972.json"
   events <- jsonlite::read_json(event_path)
-  it("Obtain minutes form event type 'Goal'", {
-    event <- list(
-      "type" = "Goal",
-      "time" = list("elapsed" = 21),
-      "team" = list("id" = 505, "name" = "Inter"),
-      "player" = list("id" = 217, "name" = "L. Martínez")
-    )
-    obtained <- get_time_if_event_is_goal(event)
-    expected <- 21
-    expect_equal(obtained, expected)
-  })
   it("Obtain minute from all 'Goal' events", {
     expected <- c(11, 21, 65)
     obtained <- get_time_from_goal_events(events)
+    expect_equal(obtained, expected)
+  })
+  event <- list(
+    "type" = "Goal",
+    "time" = list("elapsed" = 21),
+    "team" = list("id" = 505, "name" = "Inter"),
+    "player" = list("id" = 217, "name" = "L. Martínez")
+  )
+  it("Obtain minutes form event type 'Goal'", {
+    obtained <- get_time_if_event_is_goal(event)
+    expected <- 21
     expect_equal(obtained, expected)
   })
   it("Obtain team nane from all 'Goal' events", {
@@ -23,12 +23,6 @@ describe("Obtain goals from events", {
     expect_equal(obtained, expected)
   })
   it("Obtain name form event type 'Goal'", {
-    event <- list(
-      "type" = "Goal",
-      "time" = list("elapsed" = 21),
-      "team" = list("id" = 505, "name" = "Inter"),
-      "player" = list("id" = 217, "name" = "L. Martínez")
-    )
     obtained <- get_name_if_event_is_goal(event)
     expected <- "Inter"
     expect_equal(obtained, expected)
