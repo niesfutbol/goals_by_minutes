d846981f7014a3d861dac5e950b8c11c10368fa4
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-11 20:15:31 -0700

    ✅ Pass test

--- R/obtain_info_from_events.R
+++ R/obtain_info_from_events.R
@@ -2,6 +2,10 @@ get_time_from_goal_events <- function(events) {
   comprehenr::to_vec(for (event in events[["response"]]) get_time_if_event_is_goal(event))
 }
 
+get_name_from_goal_events <- function(events) {
+  comprehenr::to_vec(for (event in events[["response"]]) get_name_if_event_is_goal(event))
+}
+
 get_time_if_event_is_goal <- function(event) {
   if (event[["type"]] == "Goal") {
     return(event[["time"]][["elapsed"]])
