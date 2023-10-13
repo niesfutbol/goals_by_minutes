7d2c05e8b0ea68b77fe05a5c0d4ada3629f8d446
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-12 18:10:50 -0700

    ✅ Pass test

--- R/obtain_info_from_events.R
+++ R/obtain_info_from_events.R
@@ -1,3 +1,10 @@
+GoalsFromEvents <- R6::R6Class("GoalsFromEvents", list(
+  events = NULL,
+  initialize = function(events) {
+    self$events <- events
+  }
+))
+
 get_time_from_goal_events <- function(events) {
   comprehenr::to_vec(for (event in events[["response"]]) get_time_if_event_is_goal(event))
 }
