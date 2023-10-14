c67b25e7570fb4e0c265b869e0387d653049d256
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-14 16:36:56 -0700

    🛑🧪 Test `obtain_away_info_from_fixture()`

--- tests/testthat/test_obtain_goals_from_fixtures.R
+++ tests/testthat/test_obtain_goals_from_fixtures.R
@@ -27,4 +27,32 @@ describe("Obtain goal by half from fixture", {
     obtained <- obtain_local_info_from_fixture(fixture)
     expect_equal(expected, obtained)
   })
+  it("take away info", {
+    fixture <- list(
+      "fixture" = list("id" = 881781, "date" = "2022-08-13T16:30:00+00:00"),
+      "league" = list("id" = 135, "season" = 2022, "round" = "Regular Season - 1"),
+      "teams" = list(
+        "home" = list("id" = 498, "winner" = FALSE),
+        "away" = list("id" = 499, "winner" = TRUE)
+      ),
+      "goals" = list("home" = 0, "away" = 2),
+      "score" = list(
+        "halftime" = list("home" = 0, "away" = 1),
+        "fulltime" = list("home" = 0, "away" = 2)
+      )
+    )
+    expected <- list(
+      "fixture_id" = 881781,
+      "date" = "2022-08-13T16:30:00+00:00",
+      "league_id" = 135,
+      "season" = 2022,
+      "round" = "Regular Season - 1",
+      "team_id" = 499,
+      "home" = FALSE,
+      "score_halftime" = 1,
+      "score_fulltime" = 2
+    )
+    obtained <- obtain_away_info_from_fixture(fixture)
+    expect_equal(expected, obtained)
+  })
 })
