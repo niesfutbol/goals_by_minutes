679d2250f5c09703029e3c8830fc6c4938e479cf
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-14 16:50:48 -0700

    ♻️ Extract function `.obtain_match_info_from_fixture()`

--- R/obtain_goals_from_fixtures.R
+++ R/obtain_goals_from_fixtures.R
@@ -13,15 +13,22 @@ obtain_home_info_from_fixture <- function(fixture) {
 }
 
 obtain_away_info_from_fixture <- function(fixture) {
+  match_info <- .obtain_match_info_from_fixture(fixture)
+  away_info <- list(
+    "team_id" = fixture[["teams"]][["away"]][["id"]],
+    "home" = FALSE,
+    "score_halftime" = fixture[["score"]][["halftime"]][["away"]],
+    "score_fulltime" = fixture[["score"]][["fulltime"]][["away"]]
+  )
+  c(match_info, away_info)
+}
+
+.obtain_match_info_from_fixture <- function(fixture) {
   list(
     "fixture_id" = fixture[["fixture"]][["id"]],
     "date" = fixture[["fixture"]][["date"]],
     "league_id" = fixture[["league"]][["id"]],
     "season" = fixture[["league"]][["season"]],
-    "round" = fixture[["league"]][["round"]],
-    "team_id" = fixture[["teams"]][["away"]][["id"]],
-    "home" = FALSE,
-    "score_halftime" = fixture[["score"]][["halftime"]][["away"]],
-    "score_fulltime" = fixture[["score"]][["fulltime"]][["away"]]
+    "round" = fixture[["league"]][["round"]]
   )
 }
