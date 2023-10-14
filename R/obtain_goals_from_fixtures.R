b20ea2517fa7d3e749b28c25c71484d139e26031
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-14 16:37:08 -0700

    ✅ Pass test

--- R/obtain_goals_from_fixtures.R
+++ R/obtain_goals_from_fixtures.R
@@ -11,3 +11,17 @@ obtain_local_info_from_fixture <- function(fixture) {
     "score_fulltime" = fixture[["score"]][["fulltime"]][["home"]]
   )
 }
+
+obtain_away_info_from_fixture <- function(fixture) {
+  list(
+    "fixture_id" = fixture[["fixture"]][["id"]],
+    "date" = fixture[["fixture"]][["date"]],
+    "league_id" = fixture[["league"]][["id"]],
+    "season" = fixture[["league"]][["season"]],
+    "round" = fixture[["league"]][["round"]],
+    "team_id" = fixture[["teams"]][["away"]][["id"]],
+    "home" = FALSE,
+    "score_halftime" = fixture[["score"]][["halftime"]][["away"]],
+    "score_fulltime" = fixture[["score"]][["fulltime"]][["away"]]
+  )
+}
