465d6d47bff6c524d431a2a84eeef29eb4e6010a
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-10-14 16:39:03 -0700

    ♻️✅ Extract variable `fixture`

--- tests/testthat/test_obtain_goals_from_fixtures.R
+++ tests/testthat/test_obtain_goals_from_fixtures.R
@@ -1,5 +1,4 @@
 describe("Obtain goal by half from fixture", {
-  it("take local info", {
     fixture <- list(
       "fixture" = list("id" = 881781, "date" = "2022-08-13T16:30:00+00:00"),
       "league" = list("id" = 135, "season" = 2022, "round" = "Regular Season - 1"),
@@ -13,6 +12,7 @@ describe("Obtain goal by half from fixture", {
         "fulltime" = list("home" = 0, "away" = 2)
       )
     )
+  it("take local info", {
     expected <- list(
       "fixture_id" = 881781,
       "date" = "2022-08-13T16:30:00+00:00",
@@ -28,19 +28,6 @@ describe("Obtain goal by half from fixture", {
     expect_equal(expected, obtained)
   })
   it("take away info", {
-    fixture <- list(
-      "fixture" = list("id" = 881781, "date" = "2022-08-13T16:30:00+00:00"),
-      "league" = list("id" = 135, "season" = 2022, "round" = "Regular Season - 1"),
-      "teams" = list(
-        "home" = list("id" = 498, "winner" = FALSE),
-        "away" = list("id" = 499, "winner" = TRUE)
-      ),
-      "goals" = list("home" = 0, "away" = 2),
-      "score" = list(
-        "halftime" = list("home" = 0, "away" = 1),
-        "fulltime" = list("home" = 0, "away" = 2)
-      )
-    )
     expected <- list(
       "fixture_id" = 881781,
       "date" = "2022-08-13T16:30:00+00:00",
